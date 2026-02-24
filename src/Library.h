350301c3e848ed1c13ff33325369de6b5d62cd68
AlexsdeG <alex.remmarg@aim.com>
2026-02-24 14:50:32 +0100

    fix to run on linux

--- src/Library.h
+++ src/Library.h
@@ -1,9 +1,9 @@
 
 #pragma once
 
-#include <JuceHeader.h>
 #include "CustomLookAndFeel.h"
 #include "PlaylistComponent.h"
+#include <JuceHeader.h>
 
 //==============================================================================
 
@@ -17,143 +17,145 @@
  *
  */
 class Library : public juce::Component,
-	public juce::TableListBoxModel,
-	public juce::FileDragAndDropTarget
-{
+                public juce::TableListBoxModel,
+                public juce::FileDragAndDropTarget {
 public:
+  //==============================================================================
 
-	//==============================================================================
+  /**
+   * Class Constructor for Library, reads xml library data into class,
+   * initializes member variables and configures component details.
+   *
+   * @param AudioFormatManager reference
+   */
+  Library(juce::AudioFormatManager &_formatManager);
 
-	/**
-		* Class Constructor for Library, reads xml library data into class, initializes member variables and configures component details.
-		*
-		* @param AudioFormatManager reference
-	*/
-	Library(juce::AudioFormatManager& _formatManager);
+  /**
+   * Class destructor for Library, used to write library data to xml file
+   */
+  ~Library() override;
 
-	/**
-		* Class destructor for Library, used to write library data to xml file
-	*/
-	~Library() override;
+  //==============================================================================
 
-	//==============================================================================
+  /**
+   * @return if the selected cell is a valid selection
+   */
+  bool selectionIsValid();
 
-	/**
-		* @return if the selected cell is a valid selection
-	*/
-	bool selectionIsValid();
+  /**
+   * @return selected track object
+   */
+  track getSelectedTrack();
 
-	/**
-		* @return selected track object
-	*/
-	track getSelectedTrack();
+  /**
+   * Removes a pair element from trackFolders, or a track object from the
+   * selected pair's vector
+   */
+  void deleteItem();
 
-	/**
-		* Removes a pair element from trackFolders, or a track object from the selected pair's vector
-	*/
-	void deleteItem();
-
-	//==============================================================================
+  //==============================================================================
 
 private:
+  //==============================================================================
 
-	//==============================================================================
+  /**
+   * Paints the Library Component.
+   *
+   * @param juce::Graphics object
+   */
+  void paint(juce::Graphics &) override;
 
-	/**
-		* Paints the Library Component.
-		*
-		* @param juce::Graphics object
-	*/
-	void paint(juce::Graphics&) override;
+  /**
+   * Set bounds of member components
+   */
+  void resized() override;
 
-	/**
-		* Set bounds of member components
-	*/
-	void resized() override;
+  //==============================================================================
 
-	//==============================================================================
+  /**
+   * @return Number of rows in the library selection
+   */
+  int getNumRows() override;
 
-	/**
-		* @return Number of rows in the library selection
-	*/
-	int getNumRows() override;
+  /**
+   * Paints the row's background of the directoryComponent member.
+   *
+   * @param juce::Graphics object
+   * @param Row number
+   * @param Row width
+   * @param Row height
+   * @param If the row is selected
+   */
+  void paintRowBackground(juce::Graphics &g, int rowNumber, int width,
+                          int height, bool rowIsSelected) override;
 
-	/**
-		* Paints the row's background of the directoryComponent member.
-		*
-		* @param juce::Graphics object
-		* @param Row number
-		* @param Row width
-		* @param Row height
-		* @param If the row is selected
-	*/
-	void paintRowBackground(juce::Graphics& g, int rowNumber, int width, int height, bool rowIsSelected) override;
+  /**
+   * Paints each cell of the directoryComponent member.
+   *
+   * @param juce::Graphics object
+   * @param Row number
+   * @param Column number
+   * @param Cell width
+   * @param Cell height
+   * @param If the row is selected
+   */
+  void paintCell(juce::Graphics &g, int rowNumber, int columnId, int width,
+                 int height, bool rowIsSelected) override;
 
-	/**
-		* Paints each cell of the directoryComponent member.
-		*
-		* @param juce::Graphics object
-		* @param Row number
-		* @param Column number
-		* @param Cell width
-		* @param Cell height
-		* @param If the row is selected
-	*/
-	void paintCell(juce::Graphics& g, int rowNumber, int columnId, int width, int height, bool rowIsSelected) override;
+  /**
+   * Called when cell is clicked.
+   *
+   * @param Row number
+   * @param Column number
+   * @param juce::MouseEvent triggered by user
+   */
+  void cellClicked(int rowNumber, int columnId,
+                   const juce::MouseEvent &e) override;
 
-	/**
-		* Called when cell is clicked.
-		*
-		* @param Row number
-		* @param Column number
-		* @param juce::MouseEvent triggered by user
-	*/
-	void cellClicked(int rowNumber, int columnId, const juce::MouseEvent& e) override;
+  //==============================================================================
 
-	//==============================================================================
+  /**
+   * Called when file is dragged over component
+   *
+   * @param const juce::StringArray of files being dragged over the component
+   */
+  bool isInterestedInFileDrag(const juce::StringArray &files) override;
 
-	/**
-		* Called when file is dragged over component
-		*
-		* @param const juce::StringArray of files being dragged over the component
-	*/
-	bool isInterestedInFileDrag(const juce::StringArray& files) override;
+  /**
+   * Called when file is dropped over component
+   *
+   * @param const juce::StringArray of files being dropped over the component
+   * @param x position of files being dropped
+   * @param y position of files being dropped
+   */
+  void filesDropped(const juce::StringArray &files, int x, int y) override;
 
-	/**
-		* Called when file is dropped over component
-		*
-		* @param const juce::StringArray of files being dropped over the component
-		* @param x position of files being dropped
-		* @param y position of files being dropped
-	*/
-	void filesDropped(const juce::StringArray& files, int x, int y) override;
+  //==============================================================================
 
-	//==============================================================================
+  /// Instance of CustomLookAndFeel class.
+  CustomLookAndFeel customLookAndFeel;
 
+  /// Instance of a PlaylistComponent Class
+  PlaylistComponent playlist;
 
-	/// Instance of CustomLookAndFeel class. 
-	CustomLookAndFeel customLookAndFeel;
+  /// Reference assigned to the AudioFormatManager passed into the constructor
+  juce::AudioFormatManager &formatManager;
 
-	/// Instance of a PlaylistComponent Class
-	PlaylistComponent playlist;
+  /// Reader source for the audio url
+  std::unique_ptr<juce::AudioFormatReader> audioReader;
 
-	/// Reference assigned to the AudioFormatManager passed into the constructor
-	juce::AudioFormatManager& formatManager;
+  /// Reflects the trackFolders' elements
+  juce::TableListBox directoryComponent;
 
-	/// Reader source for the audio url
-	std::unique_ptr<juce::AudioFormatReader> audioReader;
+  /// Data structure to hold playlist folders containing track objects
+  std::vector<std::pair<juce::String, std::vector<track>>> trackFolders;
 
-	/// Reflects the trackFolders' elements
-	juce::TableListBox directoryComponent;
+  /// Selected index of the directoryComponent
+  int selectedFolderIndex = -1;
 
-	/// Data structure to hold playlist folders containing track objects
-	std::vector<std::pair<juce::String, std::vector<track>>> trackFolders;
+  /// File path to read xml data from and load the trackFolders when the
+  /// application starts
+  juce::String filePath;
 
-	/// Selected index of the directoryComponent
-	int selectedFolderIndex = -1;
-
-	/// File path to read xml data from and load the trackFolders when the application starts
-	juce::String filePath{ "C:/Otodecks/AppData/Library/Data/Resource.xml" };
-
-	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(Library)
+  JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(Library)
 };
